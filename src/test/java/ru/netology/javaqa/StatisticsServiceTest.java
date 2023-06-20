db810470e12b949b5e50c5f8f99efdf03390f1b9
leandreane <monk-86@inbox.ru>
2023-06-20 17:02:36 +0300

    first commit

--- src/test/java/ru/netology/javaqa/StatisticsServiceTest.java
+++ src/test/java/ru/netology/javaqa/StatisticsServiceTest.java
@@ -5,13 +5,23 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
-
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 5, 8, 2, 5, 3, 8, 6, 11, 15, 12};
+        long expected = 15;
 
         long actual = service.findMax(incomesInBillions);
 
